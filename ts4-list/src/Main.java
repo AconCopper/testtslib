76b42006d629100d4cddecf4995417d4ae1f01c6
acon <acon@acon-Lenovo-Flex-2-14>
2022-05-02 23:23:21 +0400

    string - front to back and back to front

--- ts4-list/src/Main.java
+++ ts4-list/src/Main.java
@@ -9,13 +9,24 @@ import org.toxsoft.core.tslib.coll.impl.ElemArrayList;
 public class Main {
 	
 	public static void main(String[] aArgs) {
-		IListBasicEdit<Integer> list = new ElemArrayList<>();
-		for(int i = 0; i < 5; i++) {
-			String input = waitEnter("Enter number #%d\n", i+1);
-			list.add(Integer.parseInt(input));
+//		IListBasicEdit<Integer> list = new ElemArrayList<>();
+//		for(int i = 0; i < 5; i++) {
+//			String input = waitEnter("Enter number #%d\n", i+1);
+//			list.add(Integer.parseInt(input));
+//		}
+//		for(int i = 0; i < list.size(); i++) {
+//			pl(list.get(i).toString());
+//		}
+		IListBasicEdit<String> list = new ElemArrayList<>();
+		for(int i = 0; i < 3; i++) {
+			String input = waitEnter("Enter string #%d\n", i+1);
+			list.add(input);
 		}
 		for(int i = 0; i < list.size(); i++) {
-			pl(list.get(i).toString());
+			pl(list.get(i));
+		}
+		for(int i = 0; i < list.size(); i++) {
+			pl(list.get(list.size()-1-i));
 		}
 	}
 }
