2232614c078b33f38a7598772ca028fc2e6ec791
acon <acon@acon-Lenovo-Flex-2-14>
2022-05-09 22:34:39 +0400

    map related bug?

--- ttt/src/ttt/Main.java
+++ ttt/src/ttt/Main.java
@@ -1,5 +1,28 @@
 package ttt;
 
+import static org.toxsoft.core.tslib.utils.TsTestUtils.p;
+import static org.toxsoft.core.tslib.utils.TsTestUtils.pl;
+import static org.toxsoft.core.tslib.utils.TsTestUtils.waitEnter;
+
 public class Main {
 
+  public static void main( String[] Argz ) {
+    TTTEngine eng = new TTTEngine();
+    for( ; eng.GameState == eng.GameState.X_MOVE || eng.GameState == eng.GameState.O_MOVE; ) {
+      pl( String.valueOf( eng.GameState ) );
+      Integer moveNum = Integer.valueOf( waitEnter( "Ячейка хода?\n" ) );
+      eng.makeAMove( moveNum );
+      for( int i = 0; i < 9; i++ ) {
+        p( String.valueOf( eng.GameMap.findByKey( i ) ) );
+      }
+      pl( "" );
+      for( int i = 0; i < 3; i++ ) {
+        for( int j = 0; j < 3; j++ ) {
+          p( String.valueOf( eng.GameMap.findByKey( 3 * i + j ) ) + " " );
+        }
+        pl( "" );
+
+      }
+    }
+  }
 }
