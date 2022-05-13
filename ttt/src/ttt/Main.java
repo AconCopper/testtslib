faf2b504f58b8e30c96f688e099a333aa1eb614e
acon <acon@acon-Lenovo-Flex-2-14>
2022-05-13 06:29:38 +0400

    Interface and UI changes

--- ttt/src/ttt/Main.java
+++ ttt/src/ttt/Main.java
@@ -6,22 +6,48 @@ import static org.toxsoft.core.tslib.utils.TsTestUtils.waitEnter;
 
 public class Main {
 
+  public static String swapCellStateToCellSign( ETttCellState cellstate ) {
+    switch( cellstate ) {
+      case EMPTY: {
+        return "-";
+      }
+      case O_SIGN: {
+        return "O";
+      }
+      case X_SIGN: {
+        return "X";
+      }
+      default:
+        throw new IllegalArgumentException( "Unexpected value: " + cellstate );
+    }
+  }
+
   public static void main( String[] Argz ) {
-    TTTEngine eng = new TTTEngine();
-    for( ; eng.GameState == eng.GameState.X_MOVE || eng.GameState == eng.GameState.O_MOVE; ) {
-      pl( String.valueOf( eng.GameState ) );
+    ITttEngine eng = new TTTEngine();
+    for( ; eng.getGameState() == EGameState.X_MOVE || eng.getGameState() == EGameState.O_MOVE; ) {
+      pl( String.valueOf( eng.getGameState() ) );
       Integer moveNum = Integer.valueOf( waitEnter( "Ячейка хода?\n" ) );
       eng.makeAMove( moveNum );
-      for( int i = 0; i < 9; i++ ) {
-        p( String.valueOf( eng.GameMap.findByKey( i ) ) );
-      }
-      pl( "" );
       for( int i = 0; i < 3; i++ ) {
         for( int j = 0; j < 3; j++ ) {
-          p( String.valueOf( eng.GameMap.findByKey( 3 * i + j ) ) + " " );
+          p( swapCellStateToCellSign( eng.getGameMap().findByKey( 3 * i + j ) ) );
         }
         pl( "" );
-
+      }
+      switch( eng.getGameState() ) {
+        case X_WIN: {
+          pl( "X won" );
+          break;
+        }
+        case O_WIN: {
+          pl( "O won" );
+          break;
+        }
+        case DRAW: {
+          pl( "Draw" );
+          break;
+        }
+        default:
       }
     }
   }
