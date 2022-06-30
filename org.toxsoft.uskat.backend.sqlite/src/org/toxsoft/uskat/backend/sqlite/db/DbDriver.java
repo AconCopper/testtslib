c687cc936c4b65ab3965e3760e680b78f0ceacb2
goga <goga@GOGAL>
2022-06-30 12:10:55 +0300

    DbDriver sketch

--- org.toxsoft.uskat.backend.sqlite/src/org/toxsoft/uskat/backend/sqlite/db/DbDriver.java
+++ org.toxsoft.uskat.backend.sqlite/src/org/toxsoft/uskat/backend/sqlite/db/DbDriver.java
@@ -3,6 +3,7 @@ package org.toxsoft.uskat.backend.sqlite.db;
 import java.io.*;
 
 import org.toxsoft.core.tslib.bricks.strid.coll.*;
+import org.toxsoft.core.tslib.bricks.strid.coll.impl.*;
 import org.toxsoft.uskat.core.api.sysdescr.dto.*;
 import org.toxsoft.uskat.core.impl.dto.*;
 
@@ -44,11 +45,15 @@ public class DbDriver
   @Override
   public IStridablesList<IDtoClassInfo> loadClassInfos() {
 
+    IStridablesListEdit<IDtoClassInfo> ll = new StridablesList<>();
+
     String content = ""; // load from DB
     IDtoClassInfo dto = DtoClassInfo.KEEPER.str2ent( content );
 
+    ll.add( dto );
+
     // TODO Auto-generated method stub
-    return null;
+    return ll;
   }
 
   @Override
